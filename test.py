6556af636171bb9dd7dc299f86157f47d92c2cbe
Harrisonhecker <harrison.hecker22@gmail.com>
2023-03-05 23:33:10 -0500

    can now webscrape and pull info

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-url='https://oxylabs.io/blog'
+url='https://www.sports-reference.com/cbb/players/zion-williamson-1.html'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
-print(soup.title)
+element = soup.find('td', {'data-stat': 'pts_per_g'})
+print(element.text)
