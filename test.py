744f2ba56930935dbd1e4624dba03bc1be0933bf
Harrisonhecker <harrison.hecker22@gmail.com>
2023-03-05 23:12:32 -0500

    use beautiful soup and requests to webscrape

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 import requests
-response = requests.get("https://oxylabs.io/")
-print(response.text)
+from bs4 import BeautifulSoup
+url='https://oxylabs.io/blog'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup.title)
