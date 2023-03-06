fa632f6bf30942d800696d1f94a6dc24b92902a0
Harrisonhecker <harrison.hecker22@gmail.com>
2023-03-05 23:53:53 -0500

    find ppg for ohio state players

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
+'''
 url='https://www.sports-reference.com/cbb/players/zion-williamson-1.html'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 element = soup.find('td', {'data-stat': 'pts_per_g'})
 print(element.text)
+'''
+url = 'https://www.sports-reference.com/cbb/schools/ohio-state/men/2023.html'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+table = soup.find('table', {'id': 'per_game'})
+body = table.find_all('td', {'data-stat': 'pts_per_g'})
+for item in body:
+    print(item.text)
